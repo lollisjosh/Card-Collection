517c864c85dcb8c465b285effe3e97cb658e6841
TelloViz <lollisjosh@csu.fullerton.edu>
2024-10-11 09:31:16 -0700

    Add import

---
+++ CardCollection/backendcontroller.py
@@ -0,0 +1,22 @@
+"""
+BackendController Class Module
+Handles any front end requests for backend interaction.
+"""
+from PySide6.QtCore import QObject, Slot, Signal
+
+from pokemontcgsdk import Card
+from pokemontcgsdk import Set
+from pokemontcgsdk import Type
+from pokemontcgsdk import Supertype
+from pokemontcgsdk import Subtype
+from pokemontcgsdk import Rarity
+
+class Example:  
+    def __init__(self):  
+        pass  
+
+    def myFunc(self):  
+        pass  
+
+    def myOtherFunc(self, parameter):  
+        pass  
