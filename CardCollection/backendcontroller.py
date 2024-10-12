56773d35289a6825589b6b6db115c21424777406
TelloViz <lollisjosh@csu.fullerton.edu>
2024-10-11 23:42:28 -0700

    Filling out Docustrings

--- CardCollection/backendcontroller.py
+++ CardCollection/backendcontroller.py
@@ -1,7 +1,4 @@
-"""
-BackendController Class Module
-Handles any front end requests for backend interaction.
-"""
+
 
 from PySide6.QtCore import QObject, Slot, Signal
 
@@ -12,24 +9,50 @@ from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
 
-class BackendController:  
+class BackendController(QObject):
+    """
+    Handles any front end requests for backend interaction.
+
+    Args:
+        QObject (_type_): Inheritance of QObject allows for the use of QT Signals and Slots
+    """
     jsonSearchResults = Signal(str)  # Signal that emits JSON string
     jsonDiscoverResults = Signal(str)  # Signal that emits JSON string
     jsonLoadResults = Signal(str)  # Signal that emits JSON string
 
     @Slot(list)
-    def requestSearch(params: list[tuple[str, str, str]]):
-        pass
+    def request_search(self, params: list[tuple[str, str, str]]):
+        """
+        Provide an interface for the front end to make search requests with the given search parameters.
+
+        Args:
+            params (list[tuple[str, str, str]]): List of search parameter tuples of the form (category, subcategory, target)
+        """
 
     @Slot(list)
-    def requestDiscover(params: list[tuple[str,str,str]]):
-        pass
+    def request_discover(self, params: list[tuple[str,str,str]]):
+        """
+        The function `requestDiscover` takes a list of tuples as input parameters.
+        
+        :param params: The `requestDiscover` method takes a list of tuples as the `params` parameter.
+        Each tuple in the list should contain three strings
+        :type params: list[tuple[str,str,str]]
+        """
 
     @Slot()
-    def requestLoadCollection():
-        pass
-
+    def request_load_collection(self):
+        """
+        The function `requestLoadCollection` is defined in Python but does not contain any code inside
+        its body.
+        """
+        
     @Slot(list)
-    def requestSaveCollection(params: list[tuple[str, str, str]]):
-        pass
-    
+    def request_cave_collection(self, params: list[tuple[str, str, str]]):
+        """
+        The function `requestSaveCollection` takes a list of tuples as input parameters.
+        
+        :param params: The `requestSaveCollection` function takes a parameter `params`, which is a list
+        of tuples. Each tuple in the list contains three strings
+        :type params: list[tuple[str, str, str]]
+        """
+        
