ed6d4e40783a505d5ecc9594bccafbb11c3ebcf5
Josh Lollis <lollisjosh@csu.fullerton.edu>
2024-10-15 17:42:51 -0700

    Update backendcontroller.py
    updated line lengths and some spacing

--- CardCollection/backendcontroller.py
+++ CardCollection/backendcontroller.py
@@ -10,26 +10,26 @@ from pokemontcgsdk import Rarity
 import inithandler
 import searchhandler
 
+
 class BackendController(QObject):
     """
     Handles any front end requests for backend interaction.
 
     Args:
-        QObject (_type_): Inheritance of QObject allows for the use of QT Signals and Slots
+         QObject: Inheritance of QObject allows for the use of QT Signals and Slots
 
     ## Note:
     The `@Slot` decorator in this code is used to define a slot function that can be connected to a
     signal in a Qt application. Slots are functions or methods that can be called in response to a
-    signal being emitted. By using the `@Slot` decorator, you are explicitly defining which functions
+    signal being emitted. By using the `@Slot` decorator, you're explicitly defining which functions
     can be connected to signals in the Qt framework. This helps in maintaining a clear separation
     between signals and slots in the codebase and ensures that the connections are set up correctly.
     """
 
-    setsResults = Signal(str) # Signal that emits JSON string
+    setsResults = Signal(str)  # Signal that emits JSON string
     searchResults = Signal(str)  # Signal that emits JSON string
     discoverResults = Signal(str)  # Signal that emits JSON string
     loadResults = Signal(str)  # Signal that emits JSON string
-
 
     @Slot()
     def request_sets_retrieve(self):
@@ -39,30 +39,36 @@ class BackendController(QObject):
         Args:
             params none
         """
+
     @Slot(list)
     def request_search(self, params: list[tuple[str, str, str]]):
         """
         ## Description:
-            Provide an interface for the front end to make search requests with the given search parameters.
+            Provide an interface for the front end 
+            to make search requests with the given search parameters.
         Args:
-            params (list[tuple[str, str, str]]): List of search parameter tuples of the form (category, subcategory, target)
+            params (list[tuple[str, str, str]]): 
+                List of search parameter tuples of the form (category, subcategory, target)
         """
 
     @Slot(list)
     def request_discover(self, params: list[tuple[str, str, str]]):
         """
         ## Description
-            Provide an interface for the front end to make discover requests with the given parameters.
+            Provide an interface for the front end 
+            to make discover requests with the given parameters.
 
         Args:
-            params (list[tuple[str, str, str]]): List of search parameter tuples of the form (category, subcategory, target)
+            params (list[tuple[str, str, str]]): 
+                List of search parameter tuples of the form (category, subcategory, target)
         """
 
     @Slot()
     def request_load_collection(self):
         """
         ## Description
-            Provide an interface for the front end to request teh back end to load and return the user's collection.
+            Provide an interface for the front end 
+            to request teh back end to load and return the user's collection.
 
         Args:
             none: This function does not take any arguments.
@@ -72,8 +78,10 @@ class BackendController(QObject):
     def request_save_collection(self, params: list[tuple[str, str, str]]):
         """
         ## Description
-            Provide an interface for the front end to save users collection to system using the given parameters.
+            Provide an interface for the front end 
+            to save users collection to system using the given parameters.
 
         Args:
-            params (list[tuple[str, str, str]]): List of tuples of the form (category, subcategory, target) representing the users collection.
+            params (list[tuple[str, str, str]]): 
+                List of tuples of the form (category, subcategory, target)
         """
