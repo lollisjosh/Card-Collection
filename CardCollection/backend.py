4b87ed7b7bc24cf634c4a5936c7ff559c78eae6c
Christian <christian@MacBook-Pro-290.local>
2024-10-16 13:14:41 -0700

    moved backend.py to correct directory

---
+++ CardCollection/backend.py
@@ -0,0 +1,32 @@
+from pokemontcgsdk import Card, Set, RestClient
+
+RestClient.configure('12345678-1234-1234-1234-123456789ABCD')
+
+class Backend:
+    def construct_query(self, search_parameters: list[tuple]) -> str:
+        """
+        Constructs a query string from the given search parameters.
+        Args:
+            search_parameters (list[tuple]): A list of tuples representing the search parameters.
+              
+        Returns:
+            str: The constructed query string.
+        """
+
+    def query_api(self, query: str) -> list[Card]:
+        """
+        Queries the Pokemon TCG API using the constructed query string.
+        Args: 
+            query (str): The query string to spend to the API.
+            
+        Returns: 
+            list[Card]: A list of Card objects returned by the API.
+        """
+    
+    def all_sets(self) -> list[Set]:
+        """
+        Retrieves all available sets from the Pokemon TCG API.
+        
+        Returns:
+            list[Set]: A list of Set objects retrieved from the API.
+        """
