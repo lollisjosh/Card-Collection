bdcee8dc995cacf2f825663eab774527e585ddba
Batu Demirtas <batu@batu-8.local>
2024-10-21 19:34:49 -0700

    Modified the docstring slightly, better formatting.

--- CardCollection/searchhandler.py
+++ CardCollection/searchhandler.py
@@ -2,14 +2,15 @@ from backend import Backend
 
 class SearchHandler:
     """
-    A class that handles search operations by constructing queries and retrieving results.
+    The SearchHandler class performs the heavy lifting for the searching functionality. This class contains the method handle_search, which calls methods from the Backend class to execute the search requested by the user.
 
     Methods:
         handle_search(search_parameters): Constructs a query string using the Backend class, construct_query function.
     """
+
     def handle_search(self, search_parameters: list[tuple[str, str, str]]) -> list[Card]:
         """
-        Constructs a query string using the Backend class, construct_query function.
+        A function that takes a list of tuples representing search parameters, constructs a query string using the Backend class, and retrieves a list of Card objects from the API.
 
         Args:
             search_parameters (List[Tuple[str, str, str]]): A list of tuples where each tuple contains:
