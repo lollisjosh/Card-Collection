2570d9f231fbd8a1d754fe971facc2b13ba04b57
TelloViz <lollisjosh@csu.fullerton.edu>
2024-10-25 12:00:19 -0700

    linted and commented

--- CardCollection/cardprocessor.py
+++ CardCollection/cardprocessor.py
@@ -1,24 +1,64 @@
+from pokemonttcgsdk import Card
+
 class CardProcessor:
+    """Processes Pokémon TCG cards for easier handling and presentation.
+
+    This class provides static methods to process various attributes of Pokémon cards,
+    including abilities, attacks, and subtypes, transforming them into a structured format
+    for use in the application.
+
+    Returns:
+        None
+    """
+
     @staticmethod
     def process_cards(cards):
+        """
+        Processes a list of Pokémon cards and 
+        extracts relevant attributes.
+
+        Args:
+            cards (list[Card]): A list of Card objects 
+            from the Pokémon TCG SDK.
+
+        Returns:
+            list[dict]: A list of dictionaries, each 
+            containing processed information for a card, 
+            including its abilities, attacks, and metadata.
+        """
+
         processed_cards = []
         for card in cards:
             processed_card = {
-                **CardProcessor.process_abilities(card),
-                **CardProcessor.process_attacks(card),
-                **CardProcessor.process_subtypes(card),
-                "name": card.name,
-                "supertype": card.supertype,
-                "imageUrl": card.images.large,
-                "set": card.set.name,
-                "setSymbol": card.set.images.symbol,
-                "setLogo": card.set.images.logo,
+            **CardProcessor.process_abilities(card),
+            **CardProcessor.process_attacks(card),
+            **CardProcessor.process_subtypes(card),
+            "name": card.name,
+            "supertype": card.supertype,
+            "imageUrl": card.images.large,
+            "set": card.set.name,
+            "setSymbol": card.set.images.symbol,
+            "setLogo": card.set.images.logo,
             }
+
             processed_cards.append(processed_card)
+
         return processed_cards
 
     @staticmethod
     def process_abilities(card):
+        """
+        Extracts abilities from a Pokémon card.
+
+        Args:
+            card (Card): A Card object from the Pokémon TCG SDK.
+
+        Returns:
+            dict: A dictionary containing the first two abilities 
+            of the card, if available, along with their names, 
+            text descriptions, and types.
+        """
+
         abilities = {
             "ability1Name": "",
             "ability1Text": "",
@@ -28,39 +68,81 @@ class CardProcessor:
             "ability2Type": ""
         }
 
-        if hasattr(card, 'abilities') and card.abilities:  # Check if abilities exists and is not None
+        # Check if abilities exists and is not None
+        if hasattr(card, 'abilities') and card.abilities:  
             if len(card.abilities) > 0:
-                abilities["ability1Name"] = card.abilities[0].name
-                abilities["ability1Text"] = card.abilities[0].text if hasattr(card.abilities[0], 'text') else ""
-                abilities["ability1Type"] = card.abilities[0].type if hasattr(card.abilities[0], 'type') else ""
+
+                abilities["ability1Name"] = card.abilities[0].name                
+
+                abilities["ability1Text"] = \
+                    card.abilities[0].text if hasattr(card.abilities[0], 'text') else ""
+
+                abilities["ability1Type"] = \
+                    card.abilities[0].type if hasattr(card.abilities[0], 'type') else ""
+
             if len(card.abilities) > 1:
+
                 abilities["ability2Name"] = card.abilities[1].name
-                abilities["ability2Text"] = card.abilities[1].text if hasattr(card.abilities[1], 'text') else ""
-                abilities["ability2Type"] = card.abilities[1].type if hasattr(card.abilities[1], 'type') else ""
+
+                abilities["ability2Text"] = card.abilities[1].text \
+                    if hasattr(card.abilities[1], 'text') else ""
+
+                abilities["ability2Type"] = card.abilities[1].type \
+                    if hasattr(card.abilities[1], 'type') else ""
 
         return abilities
 
     @staticmethod
     def process_attacks(card):
+        """
+        Extracts attack information from a Pokémon card.
+
+        Args:
+            card (Card): A Card object from the Pokémon TCG SDK.
+
+        Returns:
+            dict: A dictionary containing the first four attacks of the card, 
+            if available, along with their costs, names, text descriptions, 
+            damage values, and converted energy costs.
+        """
+
         attacks = {}
+
         if hasattr(card, 'attacks') and card.attacks:  # Ensure attacks exists
             for i in range(4):
+
                 attack_prefix = f"attack{i+1}"
+
                 if len(card.attacks) > i:
+
                     attack = card.attacks[i]
+
                     cost = attack.cost if hasattr(attack, 'cost') and attack.cost else []
+
                     attacks.update({
+
                         f"{attack_prefix}Cost1": cost[0] if len(cost) > 0 else "",
+
                         f"{attack_prefix}Cost2": cost[1] if len(cost) > 1 else "",
+
                         f"{attack_prefix}Cost3": cost[2] if len(cost) > 2 else "",
+
                         f"{attack_prefix}Cost4": cost[3] if len(cost) > 3 else "",
+
                         f"{attack_prefix}Cost5": cost[4] if len(cost) > 4 else "",
+
                         f"{attack_prefix}Name": attack.name,
+
                         f"{attack_prefix}Text": attack.text if hasattr(attack, 'text') else "",
-                        f"{attack_prefix}Damage": attack.damage if hasattr(attack, 'damage') else "",
-                        f"{attack_prefix}ConvertedEnergyCost": attack.convertedEnergyCost if hasattr(attack, 'convertedEnergyCost') else 0
+
+                        f"{attack_prefix}Damage": attack.damage \
+                            if hasattr(attack, 'damage') else "",
+
+                        f"{attack_prefix}ConvertedEnergyCost": attack.convertedEnergyCost \
+                            if hasattr(attack, 'convertedEnergyCost') else 0
                     })
                 else:
+
                     attacks.update({
                         f"{attack_prefix}Cost1": "",
                         f"{attack_prefix}Cost2": "",
@@ -75,7 +157,9 @@ class CardProcessor:
         else:
             # If attacks is None or empty, set defaults
             for i in range(4):
+
                 attack_prefix = f"attack{i+1}"
+
                 attacks.update({
                     f"{attack_prefix}Cost1": "",
                     f"{attack_prefix}Cost2": "",
@@ -92,6 +176,17 @@ class CardProcessor:
 
     @staticmethod
     def process_subtypes(card):
+        """
+        Extracts subtypes from a Pokémon card.
+
+        Args:
+            card (Card): A Card object from the Pokémon TCG SDK.
+
+        Returns:
+            dict: A dictionary containing up to 
+            four subtypes of the card, if available.
+        """
+
         subtypes = {
             "subtype1": "",
             "subtype2": "",
