c494e95f3c9066df13846f8a944620e5a1d2f78a
TelloViz <lollisjosh@csu.fullerton.edu>
2024-10-27 10:17:37 -0700

    updates from other branch

--- CardCollection/cardprocessor.py
+++ CardCollection/cardprocessor.py
@@ -12,6 +12,7 @@ class CardProcessor:
     """
 
     @staticmethod
+<<<<<<< Updated upstream
     def process_cards(cards):
         """
         Processes a list of Pokémon cards and 
@@ -26,10 +27,34 @@ class CardProcessor:
             containing processed information for a card, 
             including its abilities, attacks, and metadata.
         """
+=======
+    def process_cards(
+        cards: list,
+        include_id: bool = True,
+        include_name: bool = True,
+        include_supertype: bool = True,
+        include_subtypes: bool = True,
+        include_level: bool = True,
+        include_hp: bool = True,
+        include_types: bool = True,
+        include_rules: bool = True,
+        include_abilities: bool = True,
+        include_attacks: bool = True,
+        include_retreat_cost: bool = True,
+        include_converted_retreat_cost: bool = True,
+        include_set: bool = True,
+        include_number: bool = True,
+        include_artist: bool = True,
+        include_rarity: bool = True,
+        include_flavor_text: bool = True,
+        include_images: bool = True,
+    ) -> list[dict]:
+>>>>>>> Stashed changes
 
         processed_cards = []
         for card in cards:
             processed_card = {
+<<<<<<< Updated upstream
             **CardProcessor.process_abilities(card),
             **CardProcessor.process_attacks(card),
             **CardProcessor.process_subtypes(card),
@@ -39,6 +64,40 @@ class CardProcessor:
             "set": card.set.name,
             "setSymbol": card.set.images.symbol,
             "setLogo": card.set.images.logo,
+=======
+                **(CardProcessor.process_abilities(card) if include_abilities else {}),
+                **(CardProcessor.process_attacks(card) if include_attacks else {}),
+                **(CardProcessor.process_subtypes(card) if include_subtypes else {}),
+                **(CardProcessor.process_types(card) if include_types else {}),
+
+                "id": card.id if include_id else None,
+                "name": card.name if include_name else None,
+                "supertype": card.supertype if include_supertype else None,
+                "hp": card.hp if include_hp else None,
+                "types": card.types if include_types else [],
+                "rules": card.rules if include_rules else [],
+                "retreatCost": card.retreatCost if include_retreat_cost else [],
+
+                "convertedRetreatCost": (
+                card.convertedRetreatCost if
+                    include_converted_retreat_cost else 0
+                ),
+
+                "set": card.set.name if include_set else None,
+                "number": card.number if include_number else None,
+                "artist": card.artist if include_artist else None,
+                "rarity": card.rarity if include_rarity else None,
+                "flavorText": card.flavorText if include_flavor_text else None,
+                "imageUrl": card.images.large if include_images else None,
+
+                "setSymbol": card.set.images.symbol
+                if include_images and include_set
+                else None,
+
+                "setLogo": card.set.images.logo
+                if include_images and include_set
+                else None,
+>>>>>>> Stashed changes
             }
 
             processed_cards.append(processed_card)
@@ -65,9 +124,10 @@ class CardProcessor:
             "ability1Type": "",
             "ability2Name": "",
             "ability2Text": "",
-            "ability2Type": ""
+            "ability2Type": "",
         }
 
+<<<<<<< Updated upstream
         # Check if abilities exists and is not None
         if hasattr(card, 'abilities') and card.abilities:  
             if len(card.abilities) > 0:
@@ -80,15 +140,37 @@ class CardProcessor:
                 abilities["ability1Type"] = \
                     card.abilities[0].type if hasattr(card.abilities[0], 'type') else ""
 
+=======
+        if (
+            hasattr(card, "abilities") and card.abilities
+        ):  # Check if abilities exists and is not None
+            if len(card.abilities) > 0:
+                abilities["ability1Name"] = card.abilities[0].name
+                abilities["ability1Text"] = (
+                    card.abilities[0].text if hasattr(card.abilities[0], "text") else ""
+                )
+                abilities["ability1Type"] = (
+                    card.abilities[0].type if hasattr(card.abilities[0], "type") else ""
+                )
+>>>>>>> Stashed changes
             if len(card.abilities) > 1:
 
                 abilities["ability2Name"] = card.abilities[1].name
+<<<<<<< Updated upstream
 
                 abilities["ability2Text"] = card.abilities[1].text \
                     if hasattr(card.abilities[1], 'text') else ""
 
                 abilities["ability2Type"] = card.abilities[1].type \
                     if hasattr(card.abilities[1], 'type') else ""
+=======
+                abilities["ability2Text"] = (
+                    card.abilities[1].text if hasattr(card.abilities[1], "text") else ""
+                )
+                abilities["ability2Type"] = (
+                    card.abilities[1].type if hasattr(card.abilities[1], "type") else ""
+                )
+>>>>>>> Stashed changes
 
         return abilities
 
@@ -107,8 +189,12 @@ class CardProcessor:
         """
 
         attacks = {}
+<<<<<<< Updated upstream
 
         if hasattr(card, 'attacks') and card.attacks:  # Ensure attacks exists
+=======
+        if hasattr(card, "attacks") and card.attacks:  # Ensure attacks exists
+>>>>>>> Stashed changes
             for i in range(4):
 
                 attack_prefix = f"attack{i+1}"
@@ -116,6 +202,7 @@ class CardProcessor:
                 if len(card.attacks) > i:
 
                     attack = card.attacks[i]
+<<<<<<< Updated upstream
 
                     cost = attack.cost if hasattr(attack, 'cost') and attack.cost else []
 
@@ -144,6 +231,50 @@ class CardProcessor:
                 else:
 
                     attacks.update({
+=======
+                    cost = (
+                        attack.cost if hasattr(attack, "cost") and attack.cost else []
+                    )
+                    attacks.update(
+                        {
+                            f"{attack_prefix}Cost1": cost[0] if len(cost) > 0 else "",
+                            f"{attack_prefix}Cost2": cost[1] if len(cost) > 1 else "",
+                            f"{attack_prefix}Cost3": cost[2] if len(cost) > 2 else "",
+                            f"{attack_prefix}Cost4": cost[3] if len(cost) > 3 else "",
+                            f"{attack_prefix}Cost5": cost[4] if len(cost) > 4 else "",
+                            f"{attack_prefix}Name": attack.name,
+                            f"{attack_prefix}Text": attack.text
+                            if hasattr(attack, "text")
+                            else "",
+                            f"{attack_prefix}Damage": attack.damage
+                            if hasattr(attack, "damage")
+                            else "",
+                            f"{attack_prefix}ConvertedEnergyCost": attack.convertedEnergyCost
+                            if hasattr(attack, "convertedEnergyCost")
+                            else 0,
+                        }
+                    )
+                else:
+                    attacks.update(
+                        {
+                            f"{attack_prefix}Cost1": "",
+                            f"{attack_prefix}Cost2": "",
+                            f"{attack_prefix}Cost3": "",
+                            f"{attack_prefix}Cost4": "",
+                            f"{attack_prefix}Cost5": "",
+                            f"{attack_prefix}Name": "",
+                            f"{attack_prefix}Text": "",
+                            f"{attack_prefix}Damage": "",
+                            f"{attack_prefix}ConvertedEnergyCost": 0,
+                        }
+                    )
+        else:
+            # If attacks is None or empty, set defaults
+            for i in range(4):
+                attack_prefix = f"attack{i+1}"
+                attacks.update(
+                    {
+>>>>>>> Stashed changes
                         f"{attack_prefix}Cost1": "",
                         f"{attack_prefix}Cost2": "",
                         f"{attack_prefix}Cost3": "",
@@ -152,6 +283,7 @@ class CardProcessor:
                         f"{attack_prefix}Name": "",
                         f"{attack_prefix}Text": "",
                         f"{attack_prefix}Damage": "",
+<<<<<<< Updated upstream
                         f"{attack_prefix}ConvertedEnergyCost": 0
                     })
         else:
@@ -171,10 +303,16 @@ class CardProcessor:
                     f"{attack_prefix}Damage": "",
                     f"{attack_prefix}ConvertedEnergyCost": 0
                 })
+=======
+                        f"{attack_prefix}ConvertedEnergyCost": 0,
+                    }
+                )
+>>>>>>> Stashed changes
 
         return attacks
 
     @staticmethod
+<<<<<<< Updated upstream
     def process_subtypes(card):
         """
         Extracts subtypes from a Pokémon card.
@@ -193,12 +331,27 @@ class CardProcessor:
             "subtype3": "",
             "subtype4": ""
         }
+=======
+    def process_types(card):
+        types = {"type1": "", "type2": ""}
+>>>>>>> Stashed changes
 
-        if hasattr(card, 'subtypes') and card.subtypes:  # Check if subtypes exists
+        if hasattr(card, "types") and card.types:  # Check if types exists
+            if len(card.types) > 0:
+                types["type1"] = card.types[0]  # First type
+            if len(card.types) > 1:
+                types["type2"] = card.types[1]  # Second type
+
+        return types
+
+    @staticmethod
+    def process_subtypes(card):
+        subtypes = {"subtype1": "", "subtype2": "", "subtype3": "", "subtype4": ""}
+
+        if hasattr(card, "subtypes") and card.subtypes:  # Check if subtypes exists
             subtypes["subtype1"] = card.subtypes[0] if len(card.subtypes) > 0 else ""
             subtypes["subtype2"] = card.subtypes[1] if len(card.subtypes) > 1 else ""
             subtypes["subtype3"] = card.subtypes[2] if len(card.subtypes) > 2 else ""
             subtypes["subtype4"] = card.subtypes[3] if len(card.subtypes) > 3 else ""
-
 
         return subtypes
